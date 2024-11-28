47ff042e3e999ae76b7a6af4eba7dc5c109c8733
tahmid-100 <refattahmid876@gmail.com>
2024-11-28 18:25:32 +0600

    shible

---
+++ Dog.swift
@@ -0,0 +1,13 @@
+//
+//  Dog.swift
+//  firstApp
+//
+//  Created by Ahanaf Tahmid Rifat on 27/11/24.
+//
+
+import SwiftUI
+
+struct Dog: Identifiable {
+    var id: String
+    var breed: String
+}
